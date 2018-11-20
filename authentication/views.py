3e9b2c538dcc9543ba59057eec603f4e57cb8b83
Soheil <s_khodayari@outlook.com>
2018-11-20 20:15:29 +0100

    registration and user data model done

--- authentication/views.py
+++ authentication/views.py
@@ -1,22 +1,54 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.decorators.http import require_POST
+from django.core.urlresolvers import reverse_lazy
+from crowdea.utility import reverse_with_query
 
 # Create your views here.
 
 def getLogin(request):
 	return render(request, "authentication/login.html", {})
 
+@require_POST
 def postLogin(request):
 	username = request.POST["email"]
 	password = request.POST["password"]
 
 	return HttpResponse(username + password)
 
+
 def getRegister(request):
 	return render(request, "authentication/register.html", {})
 
+
+@require_POST
 def postRegister(request):
-	pass
+
+	username = request.POST.get("username", "")
+	password = request.POST.get("password", "")
+	firstname = request.POST.get("firstname", "")
+	lastname = request.POST.get("lastname", "")
+
+	if username == "" or password == "" or firstname == "" or lastname == "":
+		query_kwargs = {"Reg-Error":"Incomplete-Data"}
+		return HttpResponseRedirect(reverse_with_query("authApp:getRegister",query_kwargs))
+	
+	user, created = User.objects.get_or_create(username=username)
+	if created:
+		user.password = password
+		user.first_name = firstname
+		user.last_name = last_name
+		user.save()
+		return render(request, "index.html", {"user": user})
+	else:
+		query_kwargs = {"Reg-Error":"Error-Username-Exists"}
+		return HttpResponseRedirect(reverse_with_query("authApp:getRegister", query_kwargs))	
+
+
+
+
+
---
+++ crowdea/utility.py
@@ -0,0 +1,11 @@
+from django.utils.http import urlencode
+from django.core.urlresolvers import reverse_lazy
+
+def reverse_with_query(viewname, query_kwargs=None):
+
+    url = reverse_lazy(viewname)
+    if query_kwargs:
+    	msg = urlencode(query_kwargs)
+        return "%s?%s"%(url, msg)
+
+    return url
