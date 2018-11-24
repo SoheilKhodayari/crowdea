584a95247d7a70b1bf72cdb28a44ad9fae9223c6
Soheil <s_khodayari@outlook.com>
2018-11-24 12:14:47 +0100

    Idea Model + Landing Page

---
+++ idea/views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+
+
+
+def getAddIdea(request):
+	pass
+
+def postAddIdea(request):
+	pass
