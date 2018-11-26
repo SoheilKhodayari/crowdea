a4ec54572776a017b69861b98369c3f5acfaa3d9
Sabina Fataliyeva <sabina.fataliyeva@gofore.com>
2018-11-26 23:14:42 +0100

    controllers for view idea(s) + tests

--- idea/urls.py
+++ idea/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     url(r'^get-add-idea$', views.getAddIdea, name="getAddIdea"),
     url(r'^add-idea$', views.postAddIdea, name="postAddIdea"),
     url(r'^get-all-ideas$', views.getAllIdeas, name="getAllIdeas"),
-    url(r'^get-idea/(\d+)/$', views.getIdeaById, name="getIdeaById")#,
-    #url(r'^get-filter-ideas$', views.getFilterIdeas, name="getFilterIdeas"),
-    #url(r'^get-filtered-ideas(?P<keyword>\w+|)$', views.getFilteredIdeas, name="getFilteredIdeas"),
+    url(r'^get-idea/(\d+)/$', views.getIdeaById, name="getIdeaById"),
+    url(r'^get-filter-ideas$', views.getFilterIdeas, name="getFilterIdeas"),
+    url(r'^get-filtered-ideas(?P<keyword>\w+|)$', views.getFilteredIdeas, name="getFilteredIdeas"),
 ]
