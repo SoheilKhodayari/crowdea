d475bea1f260bed170d211ee564d108916b696f3
Daniel Toniuc <dtoniuc@gmail.com>
2018-11-28 20:56:36 +0100

    Add db populate script and fix complete campaign model implementation

--- campaign/models.py
+++ campaign/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from idea.models import Idea
 
 class Campaign(models.Model):
 
@@ -11,15 +12,17 @@ class Campaign(models.Model):
     title = models.CharField(max_length=300, blank=True, null= True)
     idea = models.TextField(max_length=500, blank=True, null= True)
     is_active = models.BooleanField(default=True)
-    {# Fields of Campaign #}
+    # Fields of Campaign #
     description = models.CharField(max_length=1000, blank=False, null=False)
     meta_created_at = models.DateTimeField(auto_now_add=True)
     campaign_target_sum = models.IntegerField(blank=False, null=False)
     campaign_collected_sum = models.IntegerField(default=0, blank=False, null=False)
     meta_campaign_deadline = models.DateTimeField(blank=False, null=False)
 
-@classmethod
-    def create(cls, user, idea, description, c_target, c_deadline):
-    return cls(user=user, idea_ref=idea.pk, title=idea.title, \
-        idea=idea.idea, is_active=idea.is_active, description=desc, \
-        campaign_target_sum=c_target, meta_campaign_deadline=c_deadline )
+    @classmethod
+    def create(cls, user, idea, description, c_target, c_deadline, collected=0):
+        return cls(user=user, idea_ref=idea, title=idea.title, 
+                   idea=idea.idea, is_active=idea.is_active, 
+                   description=description, campaign_collected_sum=collected,
+                   campaign_target_sum=c_target, 
+                   meta_campaign_deadline=c_deadline )
